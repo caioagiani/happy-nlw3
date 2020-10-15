ee72a2f4eb6ee023b6679183f61007a791f03be8
caioagiani <agianicaio@gmail.com>
2020-10-15 13:33:41 -0300

    feat: mobile #nlw

---
+++ mobile/src/pages/OrphanageDetails.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const OrphanageDetails: React.FC = () => {
+  return <View />;
+}
+
+export default OrphanageDetails;
---
+++ mobile/src/routes.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
+
+import OrphanagesMap from "./pages/OrphanagesMap";
+import OrphanageDetails from "./pages/OrphanageDetails";
+
+const { Navigator, Screen } = createStackNavigator();
+
+const Routes: React.FC = () => {
+  return (
+    <NavigationContainer>
+      <Navigator screenOptions={{ headerShown: false }}>
+        <Screen name="OrphanagesMap" component={OrphanagesMap} />
+        <Screen name="OrphanageDetails" component={OrphanageDetails} />
+      </Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Routes;
